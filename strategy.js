a6d9bc4c83629d8fc86c1a7fc3ec9b20f474ba4e
Marty Stake <martymix@gmail.com>
2019-06-27 14:23:55 -0400

    add ability to retrieve user information in the strategy

--- strategy.js
+++ strategy.js
@@ -1,33 +1,39 @@
-const OAuth2Strategy = require('passport-oauth2').Strategy;
-const util = require('util');
+var OAuth2Strategy = require('passport-oauth2').Strategy
+  , util = require('util');
 
 
 function Strategy(options, verify) {
+
     options = options || {};
     options.authorizationURL = options.authorizationURL || 'https://app.hubspot.com/oauth/authorize';
     options.tokenURL = options.tokenURL || 'https://api.hubapi.com/oauth/v1/token';
-    options.scope = options.scope || ['contacts'];
+    options.scope = options.scope || ['contacts', 'content', 'reports', 'social', 'automation', 'timeline'];
 
     OAuth2Strategy.call(this, options, verify);
 
     this.name = 'hubspot';
+    this._skipUserProfile = false;
     this._oauth2._useAuthorizationHeaderForGET = true;
-    this._scope = options.scope;
+    this._scope = options.scope
 }
 
 util.inherits(Strategy, OAuth2Strategy);
 
-Strategy.prototype.authorizationParams = function (options) {
-  const params = {};
-  
-  if (options.optional_scope) {
-    Object.assign(params, {
-      optional_scope: options.optional_scope
-    });
-  }
 
-  return params;
-}
+/**
+ * Retrieve user profile from HubSpot.
+ *
+ * This function constructs a normalized profile, with the following properties:
+ *
+ *   - `provider`         always set to `hubspot`
+ *   - `id`
+ *   - `username`
+ *   - `displayName`
+ *
+ * @param {String} accessToken
+ * @param {Function} done
+ * @api protected
+ */
 
 Strategy.prototype.userProfile = function (accessToken, done) {
   this._oauth2.get(`https://api.hubapi.com/oauth/v1/access-tokens/${accessToken}`,
